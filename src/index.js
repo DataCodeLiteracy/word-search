50d5da4bd6f69e2aa718e0d6f1210b94f0184124
DataCodeLiteracy <dataliteracy@icloud.com>
2023-09-30 15:16:48 +0900

    feat: 라우팅 구현하기
    2023.09.30

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import Home from './pages/Home'
+import WordSearch from './pages/WordSearch'
+import WordSearchMaker from './pages/WordSearchMaker'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: 'wordSearch',
+        element: <WordSearch />
+      },
+      {
+        path: 'wordSearchMaker',
+        element: <WordSearchMaker />
+      },
+      {
+        path: 'signup',
+        element: <SignUp />
+      },
+      {
+        path: 'signin',
+        element: <SignIn />
+      }
+    ]
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
